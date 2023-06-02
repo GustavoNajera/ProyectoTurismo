c82f248793c9e12e0888bef6bcbe26573f6de0f2
Gustavo <gustavonajera1995@gmail.com>
2023-06-01 23:15:15 -0600

    Show message of loading when data is not ready

--- tevily-nextjs-main/src/pages/destinations/[destinationId].js
+++ tevily-nextjs-main/src/pages/destinations/[destinationId].js
@@ -3,29 +3,41 @@ import Layout from "@/components/Layout/Layout";
 import PageHeader from "@/components/PageHeader/PageHeader";
 import React from "react";
 import destinationsOne from "@/data/destinationsOne";
+import { useRouter } from 'next/router';
 
-const DestinationsDetails = ({destinationDetail}) => {
-  return (
-    <>
-      <Layout pageTitle={destinationDetail.title}>
-        <PageHeader title="Destinations Details" page={destinationDetail.title} />
-        <DestinationsDetailsPage destinationDetail={destinationDetail}/>
-      </Layout>
-    </>
-  );
+
+const DestinationsDetails = () => {
+  const destinationDetail = getDestinationDetail();
+  if(!destinationDetail)
+    return pageGettingData();
+  return pageDetail(destinationDetail);
 };
 
+
+const getDestinationDetail = () =>{
+  const router = useRouter();
+  const destinationId = Number(router.query.destinationId);
+  return destinationsOne.find(d => d.id === destinationId);
+};
+
+
+const pageGettingData = () => (
+  <> 
+      <Layout pageTitle="Destinations Details">
+        <PageHeader title="Destinations Details" page="Destinations Details" />
+        <p style={{margin: "50px"}}>Getting data, please wait...</p>
+      </Layout>
+    </>
+);
+
+const pageDetail = (destinationDetail) => (
+  <> 
+    <Layout pageTitle={destinationDetail.title}>
+      <PageHeader title="Destinations Details" page={destinationDetail.title} />
+      <DestinationsDetailsPage destinationDetail={destinationDetail}/>
+    </Layout>
+  </>
+);
+
+
 export default DestinationsDetails;
-
-
-export async function getServerSideProps({ params }) {
-  
-  var destinationId = Number(params.destinationId);
-  var destinationDetail = destinationsOne.find(d => d.id === destinationId);
-  
-  return {
-    props: {
-      destinationDetail
-    }
-  }
-}
