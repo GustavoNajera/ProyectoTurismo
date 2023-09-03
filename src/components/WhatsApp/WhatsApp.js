ffdaa36d6c00ff9285f3b98301832004f3507a8a
Gustavo <gustavonajera1995@gmail.com>
2023-09-02 20:30:06 -0600

    URL Instagram y btn whatsApp funcional

--- src/components/WhatsApp/WhatsApp.js
+++ src/components/WhatsApp/WhatsApp.js
@@ -1,12 +1,13 @@
 import React from "react";
 import imgWhatsApp from "@/images/resources/whatsapp.webp";
+import { Image } from "react-bootstrap";
 
 
 const WhatsApp = () => {
   return (
     <section>
-      <a target="_blank" href="https://wa.me/71114182?text=Hola!%20Estoy%20interesado%20en%20tu%20servicio">
-        <img class="btn-whatsapp" src={imgWhatsApp.src} width="64" height="64" alt="Cotiza con nosotros"/>
+      <a target="_blank" rel="noreferrer" href="https://wa.me/71114182?text=Hola!%20Estoy%20interesado%20en%20tu%20servicio">
+        <Image className="btn-whatsapp" src={imgWhatsApp.src} width="64" height="64" alt="Cotiza con nosotros"/>
       </a>
     </section>
   );
